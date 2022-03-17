e943971a5b6f0522b6831847f3bf1a1b725c97b3
AHCChan <angelo.hoi.chung.chan@gmail.com>
2022-03-17 23:51:17 +1000

    Fixed small typo in the commentary.

--- Generate_Random_Chromosomes.py
+++ Generate_Random_Chromosomes.py
@@ -521,7 +521,7 @@ def Parse_Command_Line_Input__Generate_Synthetic_Genome(raw_command_line_input):
         PRINT.printE(STR__use_help)
         return 1
     
-    # Validate mandatroy inputs
+    # Validate mandatory inputs
     path_in = inputs.pop(0)
     valid = Validate_Read_Path(path_in)
     if valid == 1:
