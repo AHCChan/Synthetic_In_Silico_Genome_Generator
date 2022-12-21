8b11f1a1f75a8c24c6b1382e2075d82dd15dbacb
Angelo Chan <angelo.hoi.chung.chan@gmail.com>
2022-12-21 23:27:07 +1000

    Begin creating automated test module.

---
+++ Automated_Test_Module.py
@@ -0,0 +1,40 @@
+HELP_DOC = """
+SISGG AUTOMATED TEST MODULE
+(version 1.0)
+by Angelo Chan
+
+This is a tool designed to ensure that core functions of the SISGG package are
+working correctly.
+
+It does not test whether or not the code works from Command Line.
+"""
+
+# Start ########################################################################
+
+print("Running automated tests for SISGG package...\n")
+
+
+
+# Missing Programs #############################################################
+
+print("Checking to make sure all files are present...\n")
+
+
+
+# Missing Dependencies #########################################################
+
+print("Checking to make sure all dependencies are present...\n")
+
+
+
+# Core Functionality ###########################################################
+
+print("Commencing testing of core functionality...\n")
+
+
+
+# Start ########################################################################
+
+print("Testing complete.\n")
+
+
